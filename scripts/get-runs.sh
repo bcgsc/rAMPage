912a98199dc93165fca933bcd269d8aebd658d0f
Diana Lin <dlin@bcgsc.ca>
2020-11-13 13:46:58 -0800

    reformat, move environ printout after checks pass

--- scripts/get-runs.sh
+++ scripts/get-runs.sh
@@ -4,7 +4,8 @@ PROGRAM=$(basename $0)
 
 function get_help() {
     # DESCRIPTION
-    { echo "DESCRIPTION:";
+    {
+        echo "DESCRIPTION:"
         echo -e "\
 		\tGets the SRA RUN (i.e. SRR) accessions using wget.\n \
 		\n \
@@ -19,54 +20,72 @@ function get_help() {
 		\t-------------\n \
 		\t  - 0: successfully completed\n \
 		\t  - 1: general error\n \
-        " | column -s$'\t' -t -L;
-        
+        " | column -s$'\t' -t -L
+
         # USAGE
-        echo "USAGE(S):";
+        echo "USAGE(S):"
         echo -e "\
 		\t$PROGRAM -o <output directory> <SRA accessions TXT file>\n \
-        " | column -s$'\t' -t -L;
-        
+        " | column -s$'\t' -t -L
+
         # OPTIONS
-        echo "OPTION(S):";
+        echo "OPTION(S):"
         echo -e "\
 		\t-h\tshow this help menu\n \
 		\t-o <directory>\toutput directory\t(required)\n \
-    " | column -s$'\t' -t -L; }
-    
+    " | column -s$'\t' -t -L
+    }
+
     exit 1
-    
+
 }
 
 function print_error() {
-    { message="$1";
-        echo "ERROR: $message";
-        printf '%.0s=' $(seq 1 $(tput cols));
-        echo;
-    get_help; } 1>&2
+    {
+        message="$1"
+        echo "ERROR: $message"
+        printf '%.0s=' $(seq 1 $(tput cols))
+        echo
+        get_help
+    } 1>&2
 }
+if [[ "$#" -eq 0 ]]; then
+    get_help
+fi
 
-while getopts :ho: opt
-do
+while getopts :ho: opt; do
     case $opt in
-        h) get_help;;
-        o) outdir=$(realpath $OPTARG); mkdir -p $outdir;;
-        \?) print_error "Invalid option: -$OPTARG";;
+    h) get_help ;;
+    o)
+        outdir=$(realpath $OPTARG)
+        mkdir -p $outdir
+        ;;
+    \?) print_error "Invalid option: -$OPTARG" ;;
     esac
 done
 
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 # if no arguments, print help menu automatically
-if [[ "$#" -eq 0 ]]
-then
+if [[ "$#" -eq 0 ]]; then
     get_help
 fi
 
 # if incorrect number of arguments is given
-if [[ "$#" -ne 1 ]]
-then
+if [[ "$#" -ne 1 ]]; then
     print_error "Incorrect number or arguments."
+fi
+
+# remove status files if alraedy exist
+rm -f $outdir/RUNS.DONE
+
+# check if input file exists or is empty
+if [[ ! -s $1 ]]; then
+    if [[ ! -f $1 ]]; then
+        print_error "ERROR: Input file $(realpath $1) does not exist."
+    else
+        print_error "Input file $(realpath $1) is empty."
+    fi
 fi
 
 # print environment details
@@ -75,46 +94,25 @@ echo -e "START: $(date)" 1>&2
 start_sec=$(date '+%s')
 
 echo -e "PATH=$PATH\n" 1>&2
-
-# remove 'completion' files if alraedy exist
-if [[ -f $outdir/RUNS.DONE ]]
-then
-    rm $outdir/RUNS.DONE
-fi
-
-# check if input file exists or is empty
-if [[ ! -s $1 ]]
-then
-    if [[ ! -f $1 ]]
-    then
-        print_error "ERROR: Input file $(realpath $1) does not exist."
-    else
-        print_error "Input file $(realpath $1) is empty."
-    fi
-    get_help
-fi
-
 accessions=$(cat $1)
 
 echo "Downloading run info..." 1>&2
-for i in $accessions
-do
-    while [[ ! -s $outdir/temp.${i}.csv ]]
-    do
+for i in $accessions; do
+    while [[ ! -s $outdir/temp.${i}.csv ]]; do
         wget --tries=inf -o "$outdir/${i}.wget.log" -O "$outdir/temp.${i}.csv" "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=${i}" || true
     done
 done
 
-cat $outdir/*.wget.log > $outdir/wget.log
+cat $outdir/*.wget.log >$outdir/wget.log
 
 # get SRR numbers
-cat $outdir/temp*.csv > $outdir/RunInfoTable.csv
+cat $outdir/temp*.csv >$outdir/RunInfoTable.csv
 
 # delete empty lines in RunInfoTable.csv
 sed -i '/^$/d' $outdir/RunInfoTable.csv
 
 echo "Downloading SRA Run accessions to $outdir/runs.txt..." 1>&2
-cut -f1 -d, $outdir/RunInfoTable.csv | sort -u | sed '/^$/d' | grep -v 'Run' > $outdir/runs.txt
+cut -f1 -d, $outdir/RunInfoTable.csv | sort -u | sed '/^$/d' | grep -v 'Run' >$outdir/runs.txt
 
 rm $outdir/temp*.csv
 rm $outdir/*.wget.log
@@ -129,17 +127,13 @@ echo 1>&2
 
 # soft link to a generic name
 default_name="$(realpath -s $(dirname $outdir)/sra)"
-if [[ "$default_name" != "$outdir" ]]
-then
+if [[ "$default_name" != "$outdir" ]]; then
     count=1
-    if [[ -d "$default_name" ]]
-    then
-        if [[ ! -h "$default_name" ]]
-        then
+    if [[ -d "$default_name" ]]; then
+        if [[ ! -L "$default_name" ]]; then
             temp="${default_name}-${count}"
-            while [[ -d "$temp" ]]
-            do
-                count=$((count+1))
+            while [[ -d "$temp" ]]; do
+                count=$((count + 1))
                 temp="${default_name}-${count}"
             done
             echo -e "Since $default_name already exists, $default_name is renamed to $temp as to not overwrite old files.\n" 1>&2
