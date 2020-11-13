4f7fcda4a085a5ac3a9507c62927a1f32b6fd73b
Diana Lin <dlin@bcgsc.ca>
2020-11-13 13:50:29 -0800

    reformat, move environ printout after checks pass

--- scripts/get-reads.sh
+++ scripts/get-reads.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 PROGRAM=$(basename $0)
 
 function get_help() {
-    { echo "DESCRIPTION:";
+    {
+        echo "DESCRIPTION:"
         echo -e "\
 		\tGets reads for one single organism, using fasterq-dump.\n \
 		\n \
@@ -19,68 +20,80 @@ function get_help() {
 		\t  - 2: failed to download\n \
 		\n \
 		\tFor more information: https://github.com/ncbi/sra-tools/wiki/HowTo:-fasterq-dump\n \
-        " | column -s$'\t' -t -L;
-        
+        " | column -s$'\t' -t -L
+
         # USAGE
-        echo "USAGE(S):";
+        echo "USAGE(S):"
         echo -e "\
 		\t$PROGRAM [OPTIONS] -o <output directory> <SRA RUN (i.e. SRR) accession list>\n \
-        " | column -s$'\t' -t -L;
-        
+        " | column -s$'\t' -t -L
+
         # OPTIONS
-        echo "OPTION(S):";
+        echo "OPTION(S):"
         echo -e "\
 		\t-a <address>\temail alert\n \
 		\t-h\tshow this help menu\n \
 		\t-o <directory>\toutput directory\t(required)\n \
 		\t-t <int>\tnumber of threads\t(default = 2)\n \
-    " | column -s$'\t' -t -L; } 1>&2
+    " | column -s$'\t' -t -L
+    } 1>&2
     exit 1
 }
-
 function print_error() {
-    { message="$1";
-        echo "ERROR: $message";
-        printf '%.0s=' $(seq 1 $(tput cols));
-        echo;
-    get_help; } 1>&2
+    {
+        message="$1"
+        echo "ERROR: $message"
+        printf '%.0s=' $(seq 1 $(tput cols))
+        echo
+        get_help
+    } 1>&2
 }
 
+if [[ "$#" -eq 0 ]]; then
+    get_help
+fi
 # default parameters
 threads=6
 email=""
-while getopts :a:ho:t: opt
-do
+while getopts :a:ho:t: opt; do
     case $opt in
-        a) address="$OPTARG"; email="-a $address" ;;
-        h) get_help;;
-        o) outdir="$(realpath $OPTARG)"; mkdir -p $outdir;;
-        t) threads="$OPTARG";;
-        \?) print_error "Invalid option: -$OPTARG";;
+    a)
+        address="$OPTARG"
+        email="-a $address"
+        ;;
+    h) get_help ;;
+    o)
+        outdir="$(realpath $OPTARG)"
+        mkdir -p $outdir
+        ;;
+    t) threads="$OPTARG" ;;
+    \?) print_error "Invalid option: -$OPTARG" ;;
     esac
 done
 
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 # no arguments given
-if [[ "$#" -eq 0 ]]
-then
+if [[ "$#" -eq 0 ]]; then
     get_help
 fi
 
 # incorrect arguments given
-if [[ "$#" -ne 1 ]]
-then
-    echo "Incorrect number of arguments."
+if [[ "$#" -ne 1 ]]; then
+    print_error "Incorrect number of arguments."
 fi
 
 # if older 'completion' files exist, remove them
-if [[ -f $outdir/READS.DONE ]]
-then
-    rm $outdir/READS.DONE
-elif [[ -f $outdir/READS.FAIL ]]
-then
-    rm $outdir/READS.FAIL
+rm -f $outdir/READS.DONE
+rm -f $outdir/READS.FAIL
+
+# check if input file is empty or does not exist
+if [[ ! -s $1 ]]; then
+    if [[ ! -f $1 ]]; then
+        print_error "Input file $1 does not exist."
+    else
+        print_error "Input file $1 is empty."
+    fi
 fi
 
 echo "HOSTNAME: $(hostname)" 1>&2
@@ -89,48 +102,32 @@ start_sec=$(date '+%s')
 
 sra=$(realpath $1)
 
-# check if input file is empty or does not exist
-if [[ ! -s $sra ]]
-then
-    if [[ ! -f $sra ]]
-    then
-        print_error "Input file $sra does not exist."
-    else
-        print_error "Input file $sra is empty."
-    fi
-fi
-
 export PATH=$(dirname $(command -v $FASTERQ_DUMP)):$PATH
 echo -e "PATH=$PATH\n" 1>&2
 
 echo "PROGRAM: $(command -v $FASTERQ_DUMP)" 1>&2
-$FASTERQ_DUMP --version > /dev/null
+$FASTERQ_DUMP --version >/dev/null
 echo -e "VERSION: $($FASTERQ_DUMP --version | awk '/version/ {print $NF}')\n" 1>&2
 
 # get each accession parallelly using the get-accession.sh script
-while read accession
-do
+while read accession; do
     # assume that the FASTQs do not exist due to timestamping of the folders
     echo "Initiating download of ${accession}..." 1>&2
     $ROOT_DIR/scripts/get-accession.sh $email -t $threads -o $outdir $accession &
-done < $sra
+done <$sra
 
 # wait for all child processes to finish
 wait
 
 # soft link to 'default name'
 default_name="$(realpath -s $(dirname $outdir)/raw_reads)"
-if [[ "$outdir" != "$default_name" ]]
-then
+if [[ "$outdir" != "$default_name" ]]; then
     count=1
-    if [[ -d "$default_name" ]]
-    then
-        if [[ ! -h "$default_name" ]]
-        then
+    if [[ -d "$default_name" ]]; then
+        if [[ ! -L "$default_name" ]]; then
             temp="${default_name}-${count}"
-            while [[ -d "$temp" ]]
-            do
-                count=$((count+1))
+            while [[ -d "$temp" ]]; do
+                count=$((count + 1))
                 temp="${default_name}-${count}"
             done
             echo -e "\nSince $default_name already exists, $default_name is renamed to $temp as to not overwrite old reads.\n" 1>&2
@@ -146,34 +143,29 @@ fail=false
 failed_accs=()
 
 # store an array of all 'failed' accessions
-while read accession
-do
-    if ls $outdir/${accession}*.fastq.gz 1> /dev/null 2>&1
-    then
+while read accession; do
+    if ls $outdir/${accession}*.fastq.gz 1>/dev/null 2>&1; then
         :
     else
         # if there are failed accessions, add to a list and fail = true
         fail=true
-        failed_accs+=( $accession )
+        failed_accs+=($accession)
     fi
-done < $sra
+done <$sra
 
 # if fail = true, then write 'FAIL' file.
-if [[ "$fail" = true ]]
-then
+if [[ "$fail" = true ]]; then
     touch $outdir/READS.FAIL
-    if [[ -f "$outdir/READS.DONE" ]]
-    then
+    if [[ -f "$outdir/READS.DONE" ]]; then
         rm $outdir/READS.DONE
     fi
-    
+
     org=$(echo "$outdir" | awk -F "/" '{print $(NF-2), $(NF-1)}')
-    if [[ ! -z $email  ]]
-    then
+    if [[ ! -z $email ]]; then
         echo "${outdir}: ${failed_accs[*]}" | mail -s "Failed downloading reads for $org" "$address"
         echo "Email alert sent to $address." 1>&2
     fi
-    
+
     echo "Failed to download: ${failed_accs[*]}" 1>&2
     echo "STATUS: failed." 1>&2
     exit 2
@@ -182,8 +174,7 @@ fi
 workdir=$(dirname $outdir)
 
 # write a file to indicate whether reads are SINGLE or PAIRED end
-if ls $outdir/*_?.fastq.gz 1> /dev/null 2>&1
-then
+if ls $outdir/*_?.fastq.gz 1>/dev/null 2>&1; then
     touch $workdir/PAIRED.END
 else
     touch $workdir/SINGLE.END
@@ -197,8 +188,7 @@ echo 1>&2
 touch $outdir/READS.DONE
 
 org=$(echo "$outdir" | awk -F "/" '{print $(NF-2), $(NF-1)}')
-if [[ -n $email  ]]
-then
+if [[ -n $email ]]; then
     echo "$outdir" | mail -s "Finished downloading reads for $org" "$address"
     echo "Email alert sent to $address." 1>&2
 fi
