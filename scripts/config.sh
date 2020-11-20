c74c8f40b7c48148dd71118a9e2182f6588abb1e
Diana Lin <dlin@bcgsc.ca>
2020-11-19 16:01:08 -0800

    Restore config.sh to default values

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,32 +8,32 @@ then
 fi
 
 ## FOR DOWNLOADING METADATA
-export RUN_ESEARCH=/gsc/btl/linuxbrew/bin/esearch
-export RUN_EFETCH=/gsc/btl/linuxbrew/bin/efetch
+export RUN_ESEARCH=$ROOT_DIR/src/edirect/13.8/bin/esearch
+export RUN_EFETCH=$ROOT_DIR/src/edirect/13.8/bin/efetch
 
 ## FOR DOWNLOADING READS
 export FASTERQ_DUMP=$ROOT_DIR/src/sratoolkit.2.10.5-centos_linux64/bin/fasterq-dump
 
 ## FOR TRIMMING READS
-export RUN_FASTP=/gsc/btl/linuxbrew/bin/fastp
+export RUN_FASTP=$ROOT_DIR/fastp/0.20.1/bin/fastp
 
 ## FOR ASSEMBLY
 export RUN_RNABLOOM=$ROOT_DIR/src/RNA-Bloom_v1.3.1/RNA-Bloom.jar
 export NTCARD_DIR=$ROOT_DIR/src/ntCard
-export MINIMAP_DIR=/home/kmnip/programs/minimap2-2.17_x64-linux
-export JAVA_EXEC=/home/kmnip/jdk1.8.0_101/jre/bin/java # needs to be at least 1.8
-export RUN_CDHIT=/gsc/btl/linuxbrew/bin/cd-hit
-export RUN_SEQTK=/gsc/btl/linuxbrew/bin/seqtk
+export MINIMAP_DIR=$ROOT_DIR/src/minimap2-2.17_x64-linux
+export JAVA_EXEC=$ROOT_DIR/src/jdk1.8.0_101/jre/bin/java # needs to be at least 1.8
+export RUN_CDHIT=$ROOT_DIR/src/cd-hit/4.8.1_1/bin/cd-hit
+export RUN_SEQTK=$ROOT_DIR/src/seqtk/1.1/bin/seqtk
 
 ## QUANTIFY
 export RUN_SALMON=$ROOT_DIR/src/salmon-latest_linux_x86_64/bin/salmon
 
 ## TRANSLATE
-export TRANSDECODER_LONGORFS=/gsc/btl/linuxbrew/bin/TransDecoder.LongOrfs
-export TRANSDECODER_PREDICT=/gsc/btl/linuxbrew/bin/TransDecoder.Predict
+export TRANSDECODER_LONGORFS=$ROOT_DIR/src/transdecoder/5.5.0/bin/TransDecoder.LongOrfs
+export TRANSDECODER_PREDICT=$ROOT_DIR/src/transdecoder/5.5.0/bin/TransDecoder.Predict
 
 ## HOMOLOGY SEARCH
-export RUN_JACKHMMER=/gsc/btl/linuxbrew/bin/jackhmmer
+export RUN_JACKHMMER=$ROOT_DIR/src/hmmer/3.3.1/bin/jackhmmer
 
 ## CLEAVAGE
 export RUN_SIGNALP=$ROOT_DIR/src/signalp-3.0/signalp
@@ -44,6 +44,6 @@ export RUN_AMPLIFY=$ROOT_DIR/src/AMPlify-1.0.0/src/AMPlify.py
 
 ## SABLE
 export RUN_SABLE=$ROOT_DIR/src/sable_v4_distr/run.sable
-export BLAST_BIN=/gsc/btl/linuxbrew/Cellar/blast/2.10.0/bin
+export BLAST_DIR=$ROOT_DIR/src/blast/2.10.0/bin
 
 touch $ROOT_DIR/CONFIG.DONE
