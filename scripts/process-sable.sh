f479db3cb374d21c413f4dbf4b029e96c3701631
Diana Lin <dlin@bcgsc.ca>
2021-01-13 15:45:38 -0800

    add support for empty annotation

--- scripts/process-sable.sh
+++ scripts/process-sable.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 PROGRAM=$(basename $0)
-
+args="$PROGRAM $*"
 # 0 - table function
 function table() {
 	if column -L <(echo) &>/dev/null; then
@@ -11,7 +11,6 @@ function table() {
 		echo
 	fi
 }
-
 # 1 - get_help
 function get_help() {
 	{
@@ -22,12 +21,17 @@ function get_help() {
 
 		echo "USAGE(S):"
 		echo -e "\
-		\t$PROGRAM [OPTIONS] <SABLE Output TXT file>\n \
+		\t$PROGRAM [OPTIONS] <SABLE query FASTA file> <SABLE Output TXT file> <AMPlify TSV file>\n \
 		" | table
 
 		echo "OPTION(S):"
 		echo -e "\
 		\t-h\tshow this help menu\n \
+		" | table
+
+		echo "EXAMPLE(S):"
+		echo -e "\
+		\t$PROGRAM /path/to/sable/OUT_SABLE_graph /path/to/amplify/AMPlify.final.tsv\n \
 		" | table
 	} 1>&2
 	exit 1
@@ -62,31 +66,53 @@ done
 shift $((OPTIND - 1))
 
 # 5 - incorrect arguments
-if [[ "$#" -ne 1 ]]; then
+if [[ "$#" -ne 3 ]]; then
 	print_error "Incorrect number of arguments."
 fi
 
-# 6 check input files
-if [[ ! -f $(realpath $1) ]]; then
-	print_error "Input file $(realpath $1) does not exist."
-elif [[ ! -s $(realpath $1) ]]; then
-	print_error "Input file $(realpath $1) is empty."
+# {
+# 	echo "HOSTNAME: $(hostname)"
+# 	echo -e "START: $(date)\n"
+#
+# 	echo -e "PATH=$PATH\n"
+#
+# 	echo "CALL: $args (wd: $(pwd))"
+# } 1>&2
+
+fasta=$(realpath $1)
+if [[ ! -s $fasta ]]; then
+	if [[ ! -f $fasta ]]; then
+		print_error "Input file does not exist."
+	else
+		print_error "Input file is empty!"
+	fi
 fi
 
-infile=$(realpath $1)
+infile=$(realpath $2)
+# 6 check input files
+if [[ ! -s $infile ]]; then
+	if [[ ! -f $infile ]]; then
+		print_error "Input file does not exist."
+	else
+		print_error "Input file is empty!"
+	fi
+fi
+
+amplify_tsv=$(realpath $3)
+if [[ ! -s $amplify_tsv ]]; then
+	if [[ ! -f $amplify_tsv ]]; then
+		print_error "Input file does not exist."
+	else
+		print_error "Input file is empty!"
+	fi
+elif [[ "$amplify_tsv" != *.tsv ]]; then
+	print_error "Input file is not a TSV file."
+fi
+
 outdir=$(dirname $infile)
 outfile=$outdir/SABLE_results.tsv
 
-if [[ ! -v WORKDIR ]]; then
-	workdir=$(dirname $outdir)
-else
-	workdir=$(realpath $WORKDIR)
-fi
-
-amplify_tsv=$workdir/amplify/AMPlify_results.conf.short.charge.nr.tsv
-echo $infile $outdir $outfile $workdir $amplify_tsv
-# amplify_tsv=$(dirname $(dirname $infile))/rr/AMPlify_results.tsv
-echo -e "Sequence ID\tSequence\tScore\tCharge\tStructure\tStructure Confidence\tRSA\tRSA Confidence\tAlpha Helix\tLongest Helix\tBeta Strand\tLongest Strand" >$outfile
+echo -e "Sequence ID\tSequence\tAnnotation\tScore\tCharge\tStructure\tStructure Confidence\tRSA\tRSA Confidence\tAlpha Helix\tLongest Helix\tBeta Strand\tLongest Strand" >$outfile
 while read line; do
 	seqname=$(echo "$line" | awk '{print $2}')
 	read sequence
@@ -94,10 +120,14 @@ while read line; do
 	read str_conf
 	read rsa
 	read rsa_conf
-
+	updated_seqname=$(echo "$seqname" | sed 's/_novel//' | sed 's/_annotated//' | sed 's/_known//')
 	# get AMPlify score and charge
-	score=$(grep -F "$seqname" $amplify_tsv | cut -d$'\t' -f4)
-	charge=$(grep -F "$seqname" $amplify_tsv | cut -d$'\t' -f6)
+	score=$(grep -F "$updated_seqname" $amplify_tsv | cut -d$'\t' -f4)
+	charge=$(grep -F "$updated_seqname" $amplify_tsv | cut -d$'\t' -f6)
+	annotation=$(grep -F "${seqname} " $fasta | grep -Eo "exonerate=\S+|diamond=\S+" | tr ' ' '\n' | cut -f2 -d= | tr '\n' ' ' || true)
+	if [[ -z $annotation ]]; then
+		annotation=""
+	fi
 	ss=$($ROOT_DIR/scripts/longest-ss.py "$structure")
-	echo -e "$seqname\t$sequence\t$score\t$charge\t$structure\t${str_conf}\t${rsa}\t${rsa_conf}\t${ss}" >>$outfile
+	echo -e "$seqname\t$sequence\t$annotation\t$score\t$charge\t$structure\t${str_conf}\t${rsa}\t${rsa_conf}\t${ss}" >>$outfile
 done <$infile
